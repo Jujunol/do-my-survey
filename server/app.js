b3e039e3c95b19be59fd09cdf62bcc9b42f17774
Jujunol <johnallan4875@gmail.com>
2016-03-31 22:58:18 -0400

    Setup for project

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+/// <reference path="../typings/tsd.d.ts" />
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,3 +1,5 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
 var express = require('express');
 var router = express.Router();
 
