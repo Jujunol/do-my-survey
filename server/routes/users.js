7b861b5f750c7b0b9e0a40ec0de0f622581fd979
Jujunol <johnallan4875@gmail.com>
2016-04-22 20:29:09 -0400

    Uhhhh... a lot of changes

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var User = require('../models/user');
+
 var router = express.Router();
 
 router.get('/profile', function(req, res) {
@@ -12,6 +13,30 @@ router.get('/profile', function(req, res) {
     res.render('user/profile', {
         title: 'My Profile - Do My Survey',
         user: req.user
+    });
+});
+
+router.post('/profile', function(req, res) {
+    if(!req.user) {
+        res.redirect('/');
+        res.end();
+    }
+    User.findByIdAndUpdate(req.user._id, {
+        username: req.body.username
+    }, function(err, user) {
+        if(err) {
+            // console.log(err);
+        }
+        return res.redirect("/");
+    });
+});
+
+router.get('/:id/delete', function(req, res) {
+    if(!req.user) {
+        return res.redirect('/');
+    }
+    User.findByIdAndRemove(req.params.id, function(err, user) {
+        return res.redirect('/');
     });
 });
 
